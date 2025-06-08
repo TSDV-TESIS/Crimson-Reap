fbb9137cea8bdf8bc45d7c2948bb0f53b54492a7
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-06-08 17:22:48 -0300

    Add wind up to enemy attack

--- Assets/Scripts/Enemy/EnemyAttackController.cs
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Events;
 using Player.Properties;
 using Unity.Behavior;
 using UnityEngine;
@@ -11,42 +12,66 @@ namespace Enemy
     public class EnemyAttackController : MonoBehaviour
     {
         [SerializeField] private GameObject attackObject;
+        [SerializeField] private GameObject windUpAttackAreaObject;
         [SerializeField] private PlayerTransform playerTransform;
-        [SerializeField] private float enemySpeedInAttack = 2f;
-        [SerializeField] private float secondsInAttack = 2f;
-        
+        [SerializeField] private EnemyAttackProperties enemyAttackProperties;
+
         private Coroutine _attackCoroutine;
 
         private NavMeshAgent _navMeshAgent;
-        private EnemyAgent _enemyAgent;
+        private MeshRenderer _attackRenderer;
+        private MeshRenderer _windUpRenderer;
 
         private float _timeDiff = 0f;
+
         private void OnEnable()
         {
             _navMeshAgent ??= GetComponent<NavMeshAgent>();
-            _enemyAgent ??= GetComponent<EnemyAgent>();
+            _attackRenderer ??= attackObject?.GetComponent<MeshRenderer>();
+            _windUpRenderer ??= windUpAttackAreaObject?.GetComponent<MeshRenderer>();
         }
 
         public void StartAttack()
         {
             _timeDiff = 0f;
         }
-        
+
         public Node.Status Attack()
         {
-            attackObject.SetActive(true);
-            _navMeshAgent.destination = playerTransform.playerTransform.position;
-            _navMeshAgent.speed = enemySpeedInAttack;
+            if (_attackRenderer) _attackRenderer.enabled = enemyAttackProperties.shouldShowAttackAreaDebug;
+            if (_windUpRenderer) _windUpRenderer.enabled = enemyAttackProperties.shouldShowAttackAreaDebug;
 
-            
-            while (_timeDiff < secondsInAttack)
+            _navMeshAgent.destination = playerTransform.playerTransform.position;
+            _navMeshAgent.speed = enemyAttackProperties.enemySpeedInAttack;
+            windUpAttackAreaObject.SetActive(true);
+
+            if (IsRunning(enemyAttackProperties.attackWindupSeconds)) return Node.Status.Running;
+            windUpAttackAreaObject.SetActive(false);
+            attackObject.SetActive(true);
+
+            if (IsRunning(enemyAttackProperties.attackWindupSeconds + enemyAttackProperties.attackIFrameSeconds))
+                return Node.Status.Running;
+
+            attackObject.SetActive(false);
+            windUpAttackAreaObject.SetActive(true);
+
+            if (IsRunning(enemyAttackProperties.attackWindupSeconds + enemyAttackProperties.attackIFrameSeconds +
+                          enemyAttackProperties.attackWindoffSeconds))
+                return Node.Status.Running;
+
+            windUpAttackAreaObject.SetActive(false);
+            return Node.Status.Success;
+        }
+
+        private bool IsRunning(float seconds)
+        {
+            if (_timeDiff < seconds)
             {
                 _timeDiff += Time.deltaTime;
-                return Node.Status.Running;
+                return true;
             }
-            
-            attackObject.SetActive(false);
-            return Node.Status.Success;
+
+            return false;
         }
     }
 }
