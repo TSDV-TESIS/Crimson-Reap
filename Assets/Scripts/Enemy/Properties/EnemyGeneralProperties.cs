3274792af857f777978e4f23d5ce778acd8aa6b0
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-06-11 22:49:23 -0300

    Parametrized all values into manageable scriptable object

---
+++ Assets/Scripts/Enemy/Properties/EnemyGeneralProperties.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Enemy.Properties
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Enemy General Properties", fileName = "EnemyGeneralProperties")]
+    public class EnemyGeneralProperties : ScriptableObject
+    {
+        [Header("General Properties")] 
+        public float attackRange;
+        public float hearingRadius;
+
+        [Header("Scanning Properties")] 
+        public float minRotationTime;
+        public float maxRotationTime;
+
+        [Header("Investigation Properties")] 
+        public float investigationTime;
+
+        [Header("Suspicious Properties")] 
+        public NavMeshData suspiciousNavmeshData;
+
+        [Header("Chase Properties")] 
+        public NavMeshData chaseNavmeshData;
+
+        [Header("Patrol Properties")] 
+        public NavMeshData patrolNavmeshData;
+        public float patrolPointWaitTimeSeconds;
+
+        [Header("Attack properties")] 
+        public float attackNoiseLevel;
+        public float attackDuration;
+        public float attackStartTime;
+        public float attackIframesDuration;
+
+        [Header("Debug")] 
+        public bool shouldDrawGizmos;
+    }
+}
