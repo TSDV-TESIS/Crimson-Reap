5fa371808f7cc886f2d44360c11f35e20189a532
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-08-16 17:04:17 -0300

    Refactor movement & Add fast dropdown

--- Assets/Scripts/Player/Properties/PlayerMovementProperties.cs
+++ Assets/Scripts/Player/Properties/PlayerMovementProperties.cs
@@ -26,6 +26,10 @@ namespace Player.Properties
         public float maxJumpVelocity;
         public float cornerCorrectionMaxDistance;
         public LayerMask whatIsCeiling;
+        
+        [Header("Falling properties")]
+        public float addedDownVelocity;
+        public float addedDownGravity;
 
         [Header("Grounding properties")]
         [Tooltip("Distance from where it should start checking that player is grounded")]
