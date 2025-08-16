6703e9d5abba1ca60ebbebc55edfd9a7b42d1225
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-08-16 15:49:41 -0300

    Fix jester dash trail

--- Assets/Scripts/Player/Materials/HandleShadowstepMaterialAndTrail.cs
+++ Assets/Scripts/Player/Materials/HandleShadowstepMaterialAndTrail.cs
@@ -1,4 +1,6 @@
 using System;
+using Events.Scriptables;
+using Player.Properties;
 using UnityEngine;
 
 namespace Player.Materials
@@ -8,6 +10,9 @@ namespace Player.Materials
         [SerializeField] private Material material;
         [SerializeField] private TrailRenderer trailRenderer;
         [SerializeField] private TrailRenderer otherTrailRenderer;
+        [SerializeField] private PlayerMovementProperties playerMovementProperties;
+        
+        [Header("Events")] [SerializeField] private FloatEventChannel onShadowStepCooldown; 
         
         private static readonly int EmissionBool = Shader.PropertyToID("_Emission_Bool");
         private static readonly int IsFrenetic = Shader.PropertyToID("_is_Frenetic");
@@ -17,6 +22,28 @@ namespace Player.Materials
             SetShadowStepMaterial(false);
             trailRenderer.emitting = false;
             otherTrailRenderer.emitting = false;
+            
+            onShadowStepCooldown.onFloatEvent.AddListener(HandleShadowStepCooldown);
+        }
+
+        private void OnDisable()
+        {
+            onShadowStepCooldown.onFloatEvent.RemoveListener(HandleShadowStepCooldown);
+        }
+
+        private void HandleShadowStepCooldown(float percentageFilled)
+        {
+            if (Mathf.Approximately(percentageFilled, 1.0f))
+            {
+                SetShadowStepMaterial(false);
+                trailRenderer.emitting = false;
+                otherTrailRenderer.emitting = false;
+                return;
+            }
+            
+            float timeValue = playerMovementProperties.shadowStepShowTrailTime * (1 - percentageFilled);
+            trailRenderer.time = timeValue;
+            otherTrailRenderer.time = timeValue;
         }
 
         public void SetShadowStepMaterial(bool value)
@@ -24,8 +51,14 @@ namespace Player.Materials
             int valueToUse = value ? 1 : 0;
             material.SetInt(EmissionBool, valueToUse);
             material.SetInt(IsFrenetic, valueToUse);
-            trailRenderer.emitting = value;
-            otherTrailRenderer.emitting = value;
+
+            if (value)
+            {
+                trailRenderer.emitting = true;
+                otherTrailRenderer.emitting = true;
+                trailRenderer.time = playerMovementProperties.shadowStepShowTrailTime;
+                otherTrailRenderer.time = playerMovementProperties.shadowStepShowTrailTime;
+            }
         }
     }
 }
