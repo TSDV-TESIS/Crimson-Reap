54764208864e78dbcc3b3751fd5293f7db4357f9
NicolasLaure <nicolasariellaure@gmail.com>
2025-10-28 21:44:58 -0300

    Add menu input panel

--- Assets/Scripts/UI/PlayHandler.cs
+++ Assets/Scripts/UI/PlayHandler.cs
@@ -1,24 +1,14 @@
-using Events.Scriptables;
-using Player;
-using TMPro;
 using UnityEngine;
 
 namespace UI
 {
     public class PlayHandler : MonoBehaviour
     {
-        [SerializeField] private StringEventChannelSO onLoadSceneHandler;
-        [SerializeField] private PlayerName playerName;
-        [SerializeField] private string firstLevelName;
-        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private GameObject inputField;
 
         public void OnClick()
         {
-            if (inputField.text != "")
-            {
-                playerName.playerName = inputField.text;
-                onLoadSceneHandler?.RaiseEvent(firstLevelName);
-            }
+            inputField.SetActive(true);
         }
     }
 }
