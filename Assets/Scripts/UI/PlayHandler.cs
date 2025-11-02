b41bceff18022b2dd59fc79d7041123ad30236b8
Emanuel Apaza <emanuel.apaza.200325@gmail.com>
2025-11-02 20:59:02 -0300

    Feat: Main Menu Update UI
    Modify MainMenu Scene for update new UI.

--- Assets/Scripts/UI/PlayHandler.cs
+++ Assets/Scripts/UI/PlayHandler.cs
@@ -7,6 +7,7 @@ namespace UI
 {
     public class PlayHandler : MonoBehaviour
     {
+        [SerializeField] private GameObject inputPane_main;
         [SerializeField] private GameObject inputPanel;
         [SerializeField] private GameObject inputField;
         [SerializeField] private GameObjectEventChannelSO onNewSelectedObject;
@@ -19,12 +20,14 @@ namespace UI
 
         public void OnClick()
         {
+            inputPane_main.SetActive(true);
             inputPanel.SetActive(true);
             onNewSelectedObject?.RaiseEvent(inputField);
         }
 
         private void HandleCancel()
         {
+            inputPane_main.SetActive(false);
             inputPanel.SetActive(false);
             onNewSelectedObject?.RaiseEvent(gameObject);
         }
